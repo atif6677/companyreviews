474b4c31105d84533b0cd12db322d09ba98c3055
atif6677 <mohd.atif2531@gmail.com>
2025-07-24 11:41:11 +0530

    Changed folder name from 'private' to 'public' for better clarity and organization.

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../private')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/reviews', reviewRoutes);
 
